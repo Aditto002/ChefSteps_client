2ba4b47e3ca7d81642e16fbc543ca25d92dedf1a
Aditto Koiri <adittokoiri002@gmail.com>
2023-05-02 20:15:56 +0600

    card

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,38 @@
 import React from 'react'
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <Container className="my-5">
-      <h1 className="text-center">Login</h1>
-      <Form >
-        <Form.Group controlId="formEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
-        </Form.Group>
+    <Card className="w-100 h-100 mt-5 mb-5 mx-auto" style={{ maxWidth: '600px', maxHeight: '800px' }}>
+      <Card.Body className="d-flex flex-column justify-content-center mt-5 mb-5 ">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
 
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password"  onChange={(e) => setPassword(e.target.value)} />
-        </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Container>
+          <Button variant="primary" type="submit" block>
+            Login
+          </Button>
+
+          <Button variant="danger" className="my-4 ms-2" block>
+            Sign in with Google
+          </Button>
+
+          <Button variant="dark" className="mb-1 ms-2" block>
+            Sign in with Github
+          </Button>
+          <br />
+          <Link to='/register'>Register?</Link>
+        </Form>
+      </Card.Body>
+    </Card>
 )}
 
 export default Login
